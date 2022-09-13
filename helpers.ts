cb836db85f20fc6d5ad4ff4bde93ce9e0624ec5d
Miles Johnson <mileswjohnson@gmail.com>
2022-09-12 17:42:51 -0700

    Create helpers file.

---
+++ helpers.ts
@@ -0,0 +1,112 @@
+import type { RunReport, Duration, Action, ActionStatus } from '@moonrepo/types';
+
+export function getIconForStatus(status: ActionStatus): string {
+	switch (status) {
+		case 'cached':
+			return '🟪';
+		case 'failed':
+		case 'failed-and-abort':
+			return '🟥';
+		case 'invalid':
+			return '🟨';
+		case 'passed':
+			return '🟩';
+		default:
+			return '⬜️';
+	}
+}
+
+export function hasFailed(status: ActionStatus): boolean {
+	return status === 'failed' || status === 'failed-and-abort';
+}
+
+export function hasPassed(status: ActionStatus): boolean {
+	return status === 'passed' || status === 'cached';
+}
+
+export function isFlaky(action: Action): boolean {
+	if (!action.attempts || action.attempts.length === 0) {
+		return false;
+	}
+
+	const someAttemptsFailed = action.attempts.some((attempt) => hasFailed(attempt.status));
+
+	return hasPassed(action.status) && someAttemptsFailed;
+}
+
+export function formatDuration(duration: Duration | null): string {
+	if (!duration) {
+		return '--';
+	}
+
+	const millis = duration.nanos / 1000000;
+
+	if (duration.secs === 0) {
+		return `${millis}ms`;
+	}
+
+	let value: string[] = [];
+	let mins = 0;
+	let secs = duration.secs;
+
+	while (secs > 60) {
+		mins += 1;
+		secs -= 60;
+	}
+
+	if (mins > 0) {
+		value.push(`${mins}m`);
+	}
+
+	if (secs > 0) {
+		value.push(`${secs}s`);
+	}
+
+	if (millis > 0) {
+		value.push(`${millis}ms`);
+	}
+
+	return value.join(', ');
+}
+
+export function formatReportToMarkdown(report: RunReport): string {
+	let markdown = [
+		'### Run report',
+		'|     | Action | Time | Status |    |',
+		'| :-: | :----- | ---: | :----- | :- |',
+	];
+
+	report.actions.forEach((action) => {
+		let comments: string[] = [];
+
+		if (isFlaky(action)) {
+			comments.push('**FLAKY**');
+		}
+
+		if (action.attempts && action.attempts.length > 0) {
+			comments.push(`${action.attempts.length} attempts`);
+		}
+
+		if (hasFailed(action.status) && action.error) {
+			comments.push(action.error);
+		}
+
+		markdown.push(
+			`| ${getIconForStatus(action.status)} | \`${action.label}\` | ${formatDuration(
+				action.duration,
+			)} | ${action.status} | ${comments.join(', ')} |`,
+		);
+	});
+
+	if (report.context.touchedFiles.length > 0) {
+		markdown.push('\n', '### Touched files', '<details><summary>View files list</summary><div>');
+
+		report.context.touchedFiles.forEach((file) => {
+			markdown.push(`- ${file}`);
+		});
+
+		markdown.push('</div></details>');
+	}
+
+	return markdown.join('\n');
+}
--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import core from '@actions/core';
 import path from 'path';
 import fs from 'fs';
-import type { RunReport, Duration, Action, ActionStatus } from '@moonrepo/types';
+import type { RunReport } from '@moonrepo/types';
+import { formatReportToMarkdown } from './helpers';
 
 async function loadReport(workspaceRoot: string): Promise<RunReport | null> {
 	for (const fileName in ['ciReport.json', 'runReport.json']) {
@@ -13,117 +14,6 @@ async function loadReport(workspaceRoot: string): Promise<RunReport | null> {
 	}
 
 	return null;
-}
-
-function getIconForStatus(status: ActionStatus): string {
-	switch (status) {
-		case 'cached':
-			return '🟪';
-		case 'failed':
-		case 'failed-and-abort':
-			return '🟥';
-		case 'invalid':
-			return '🟨';
-		case 'passed':
-			return '🟩';
-		default:
-			return '⬜️';
-	}
-}
-
-function hasFailed(status: ActionStatus): boolean {
-	return status === 'failed' || status === 'failed-and-abort';
-}
-
-function hasPassed(status: ActionStatus): boolean {
-	return status === 'passed' || status === 'cached';
-}
-
-function isActionFlaky(action: Action): boolean {
-	if (!action.attempts || action.attempts.length === 0) {
-		return false;
-	}
-
-	const someAttemptsFailed = action.attempts.some((attempt) => hasFailed(attempt.status));
-
-	return hasPassed(action.status) && someAttemptsFailed;
-}
-
-function formatDuration(duration: Duration | null): string {
-	if (!duration) {
-		return '--';
-	}
-
-	const millis = duration.nanos / 1000000;
-
-	if (duration.secs === 0) {
-		return `${millis}ms`;
-	}
-
-	let value = [];
-	let mins = 0;
-	let secs = duration.secs;
-
-	while (secs > 60) {
-		mins += 1;
-		secs -= 60;
-	}
-
-	if (mins > 0) {
-		value.push(`${mins}m`);
-	}
-
-	if (secs > 0) {
-		value.push(`${secs}s`);
-	}
-
-	if (millis > 0) {
-		value.push(`${millis}ms`);
-	}
-
-	return value.join(', ');
-}
-
-function formatReportToMarkdown(report: RunReport): string {
-	let markdown = [
-		'### Run report',
-		'|     | Action | Time | Status |    |',
-		'| :-: | :----- | ---: | :----- | :- |',
-	];
-
-	report.actions.forEach((action) => {
-		let comments = [];
-
-		if (isActionFlaky(action)) {
-			comments.push('**FLAKY**');
-		}
-
-		if (action.attempts && action.attempts.length > 0) {
-			comments.push(`${action.attempts.length} attempts`);
-		}
-
-		if (hasFailed(action.status) && action.error) {
-			comments.push(action.error);
-		}
-
-		markdown.push(
-			`| ${getIconForStatus(action.status)} | \`${action.label}\` | ${formatDuration(
-				action.duration,
-			)} | ${action.status} | ${comments.join(', ')} |`,
-		);
-	});
-
-	if (report.context.touchedFiles.length > 0) {
-		markdown.push('\n', '### Touched files', '<details><summary>View files list</summary><div>');
-
-		report.context.touchedFiles.forEach((file) => {
-			markdown.push(`- ${file}`);
-		});
-
-		markdown.push('</div></details>');
-	}
-
-	return markdown.join('\n');
 }
 
 try {
